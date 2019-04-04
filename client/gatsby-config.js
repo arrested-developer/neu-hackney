c7eabf79d25380eb5c6588d9fe0da0279fd29fdb
Michael Watts <hello@michaelwatts.co>
2019-04-04 13:39:05 +0100

    replace wp environment

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
          * The base URL of the Wordpress site without the trailingslash and the protocol. This is required.
          * Example : 'gatsbyjsexamplewordpress.wordpress.com' or 'www.example-site.com'
          */
-        baseUrl: "localhost:8080",
+        baseUrl: "localhost:8001",
         // The protocol. This can be http or https.
         protocol: "http",
         // Indicates whether the site is hosted on wordpress.com.
