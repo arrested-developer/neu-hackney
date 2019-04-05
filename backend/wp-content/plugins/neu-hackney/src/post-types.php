7428b1655f98b7be032302216b564205eb1dcedb
Michael Watts <hello@michaelwatts.co>
2019-04-05 12:59:52 +0100

    add events post type relates #32 #11

--- backend/wp-content/plugins/neu-hackney/src/post-types.php
+++ backend/wp-content/plugins/neu-hackney/src/post-types.php
@@ -8,6 +8,29 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+/**
+ * Add a flag to flush permalink rewrite rules when plugin
+ * is activated
+ */
+register_activation_hook( __FILE__, 'neu_hackney_activate' );
+
+function neu_hackney_activate() {
+    if ( ! get_option( 'neu_hackney_flush_rewrite_rules_flag' ) ) {
+        add_option( 'neu_hackney_flush_rewrite_rules_flag', true );
+    }
+}
+
+add_action( 'init', 'neu_hackney_flush_rewrite_rules_maybe', 20 );
+/**
+ * Flush rewrite rules if the previously added flag exists,
+ * and then remove the flag.
+ */
+function neu_hackney_flush_rewrite_rules_maybe() {
+    if ( get_option( 'neu_hackney_flush_rewrite_rules_flag' ) ) {
+        flush_rewrite_rules();
+        delete_option( 'neu_hackney_flush_rewrite_rules_flag' );
+    }
+}
 
 /**
  * Register Post Type for Newsletters
