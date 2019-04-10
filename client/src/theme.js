664529c6c30b504e66f683a514497ca988ec89be
Michael Watts <hello@michaelwatts.co>
2019-04-10 20:28:12 +0100

    set up styled components and tachyons

---
+++ client/src/theme.js
@@ -0,0 +1,6 @@
+export default {
+  // breakpoints
+  break_ns: "screen and (min-width: 30em)",
+  break_m: "screen and (min-width: 30em) and (max-width: 60em)",
+  break_l: "screen and (min-width: 60em)",
+}
