452474d8cebda4b1184637bd75462109c3ef67fe
Michael Watts <hello@michaelwatts.co>
2019-04-15 16:30:32 +0100

    fix MapImg import relates #38

--- client/src/components/footer/locationMap.js
+++ client/src/components/footer/locationMap.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
-import MapImg from "./footer.styles"
+import { MapImg } from "./footer.styles"
 export default props => (
   <StaticQuery
     query={graphql`
