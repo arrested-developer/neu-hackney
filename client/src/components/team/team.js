782dfa682ddd181f15bab54d22e53e2215db8b4b
Michael Watts <hello@michaelwatts.co>
2019-04-18 16:46:21 +0100

    render team member image relates #6

--- client/src/components/team/team.js
+++ client/src/components/team/team.js
@@ -1,30 +1,36 @@
 import React from "react"
-import { Card, Heading, Position } from "./team.styles"
+import { Card, Heading, Position, Photo, Details } from "./team.styles"
+import Img from "gatsby-image"
+import { StaticQuery, graphql } from "gatsby"
 
-const TeamCard = ({ teamMember, position, ...props }) => (
+const TeamCard = ({ teamMember, position, queryData, ...props }) => (
   <Card {...props}>
-    <Heading>{teamMember.node.title}</Heading>
-    <Position color="black" bold>
-      {position}
-    </Position>
-    <a href={`mailto:${teamMember.node.meta.neuhack_team_member_email}`}>
-      {teamMember.node.meta.neuhack_team_member_email}
-    </a>
+    <Photo>
+      <Img fluid={queryData.headerLogo.childImageSharp.fluid} />
+    </Photo>
+    <Details>
+      <Heading>{teamMember.node.title}</Heading>
+      <Position color="black" bold>
+        {position}
+      </Position>
+      <a href={`mailto:${teamMember.node.meta.neuhack_team_member_email}`}>
+        {teamMember.node.meta.neuhack_team_member_email}
+      </a>
+    </Details>
   </Card>
 )
 
-const sortByPosition = team => {
-  const sortedTeam = team
-    .slice()
-    .sort(
-      (a, b) =>
-        a.node.meta.neuhack_team_member_position -
-        b.node.meta.neuhack_team_member_position
-    )
-  return sortedTeam
-}
-
-export default ({ team, positions, ...props }) => {
+export const AllTeamMembers = ({ team, positions, queryData, ...props }) => {
+  const sortByPosition = team => {
+    const sortedTeam = team
+      .slice()
+      .sort(
+        (a, b) =>
+          a.node.meta.neuhack_team_member_position -
+          b.node.meta.neuhack_team_member_position
+      )
+    return sortedTeam
+  }
   const positionReference = {}
   positions.edges.forEach(
     pos => (positionReference[pos.node.wordpress_id] = pos.node.name)
@@ -38,8 +44,35 @@ export default ({ team, positions, ...props }) => {
           position={
             positionReference[teamMember.node.meta.neuhack_team_member_position]
           }
+          queryData={queryData}
         />
       ))}
     </ul>
   )
 }
+
+export default ({ team, positions, ...props }) => (
+  <StaticQuery
+    query={graphql`
+      query teamQuery {
+        headerLogo: file(
+          relativePath: { eq: "neu-logo-dark-transparent.png" }
+        ) {
+          childImageSharp {
+            fluid(maxWidth: 300) {
+              ...GatsbyImageSharpFluid_noBase64
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <AllTeamMembers
+        team={team}
+        positions={positions}
+        queryData={data}
+        {...props}
+      />
+    )}
+  />
+)
--- client/src/components/team/team.styles.js
+++ client/src/components/team/team.styles.js
@@ -5,7 +5,10 @@ export const Card = styled.li`
   background: white;
   padding: ${({ theme }) => theme.m};
   margin-bottom: ${({ theme }) => theme.m};
-  ${({ theme }) => theme.box_shadow}
+  ${({ theme }) => theme.box_shadow};
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 `
 
 export const Heading = styled(H3)`
@@ -14,3 +17,13 @@ export const Heading = styled(H3)`
 `
 
 export const Position = styled(P)``
+
+export const Photo = styled.div`
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  background: ${({ theme }) => theme.dark_blue};
+  margin-right: ${({ theme }) => theme.m};
+`
+
+export const Details = styled.div``
