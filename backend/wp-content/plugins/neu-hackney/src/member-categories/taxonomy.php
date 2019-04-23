d5007207cdaa7ff96eea4b5f7627510c213a0d3d
Michael Watts <hello@michaelwatts.co>
2019-04-23 10:17:14 +0100

    add member categories and restrict editing relates #24

---
+++ backend/wp-content/plugins/neu-hackney/src/member-categories/taxonomy.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * Register taxonomy
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// register member types in the standard category taxonomy
+
+// use placeholder text as description so there is something to render
+$default_description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+function neuhack_register_categories(){
+		wp_insert_term(
+			'Teachers', // the term
+      'category', // the taxonomy
+			array(
+        'slug' => 'teachers',
+        'description' => $default_description,
+			)
+		);
+		wp_insert_term(
+			'Support Staff', // the term
+			'category', // the taxonomy
+			array(
+        'slug' => 'support-staff',
+        'description' => $default_description,
+			)
+		);
+		wp_insert_term(
+			'Supply Teachers', // the term
+			'category', // the taxonomy
+			array(
+        'slug' => 'supply-teachers',
+        'description' => $default_description,
+			)
+		);
+		wp_insert_term(
+			'School Leaders', // the term
+			'category', // the taxonomy
+			array(
+        'slug' => 'school-leaders',
+        'description' => $default_description,
+			)
+		);
+		wp_insert_term(
+			'Reps', // the term
+			'category', // the taxonomy
+			array(
+        'slug' => 'reps',
+        'description' => $default_description,
+			)
+		);
+		wp_insert_term(
+			'Post 16', // the term
+			'category', // the taxonomy
+			array(
+        'slug' => 'post-16',
+        'description' => $default_description,
+			)
+		);
+		wp_insert_term(
+			'NQTs', // the term
+			'category', // the taxonomy
+			array(
+        'slug' => 'nqts',
+        'description' => $default_description,
+			)
+		);
+		wp_insert_term(
+			'Independent Sector', // the term
+			'category', // the taxonomy
+			array(
+        'slug' => 'independent-sector',
+        'description' => $default_description,
+			)
+		);
+		wp_insert_term(
+			'Equalities', // the term
+			'category', // the taxonomy
+			array(
+        'slug' => 'equalities',
+        'description' => $default_description,
+			)
+		);
+		wp_insert_term(
+			'Academy Staff', // the term
+			'category', // the taxonomy
+			array(
+        'slug' => 'academy-staff',
+        'description' => $default_description,
+			)
+		);
+}
+add_action( 'admin_init', 'neuhack_register_categories', 0 );
+
+// set capabilities so only admin can delete a category
+
+function neuhack_set_category_capabilities() {
+  global $wp_taxonomies;
+  $capabilities = &$wp_taxonomies['category']->cap;
+  $capabilities->manage_terms = 'edit_posts';
+  $capabilities->edit_terms = 'edit_posts';
+  $capabilities->delete_terms = 'edit_users';
+  $capabilities->assign_terms = 'edit_posts';
+}
+add_action( 'init', 'neuhack_set_category_capabilities' );
