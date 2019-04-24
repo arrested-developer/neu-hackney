d3498f555acacd4ad38b1fc0c3c6c1f437779c4f
Michael Watts <hello@michaelwatts.co>
2019-04-24 21:47:41 +0100

    generate nav links from categories relates #9

--- client/src/components/layout/index.js
+++ client/src/components/layout/index.js
@@ -16,27 +16,6 @@ import Sidebar from "../sidebar"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./layout.css"
 
-const navLinks = [
-  { name: "Home", to: "/" },
-  {
-    name: "Members",
-    to: [
-      { name: "Teachers", to: "/members/teachers" },
-      { name: "NQTs", to: "/members/nqt" },
-      { name: "Support Staff", to: "/members/support-staff" },
-      { name: "Leadership", to: "/members/leadership" },
-      { name: "Post 16", to: "/members/post-16" },
-      { name: "Independent Sector", to: "/members/independent" },
-      { name: "Supply Teachers", to: "/members/supply" },
-    ],
-  },
-  { name: "Events", to: "/events" },
-  { name: "Equalities", to: "/equalities" },
-  { name: "Reps", to: "/reps" },
-  { name: "Gallery", to: "/gallery" },
-  { name: "Affiliations", to: "/affiliation" },
-]
-
 const Layout = ({ children }) => (
   <StaticQuery
     query={graphql`
@@ -46,25 +25,58 @@ const Layout = ({ children }) => (
             title
           }
         }
+        memberCats: allWordpressCategory(
+          filter: { name: { ne: "Uncategorised" } }
+        ) {
+          edges {
+            node {
+              id
+              wordpress_id
+              name
+              slug
+              count
+            }
+          }
+        }
       }
     `}
-    render={data => (
-      <div style={{ background: "teal" }}>
-        <Header siteTitle={data.site.siteMetadata.title} navLinks={navLinks} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 1200,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          {children}
-          <Sidebar />
+    render={data => {
+      const navLinks = [
+        { name: "Home", to: "/" },
+        { name: "Events", to: "/events" },
+        {
+          name: "Members",
+          to: data.memberCats.edges.map(cat => ({
+            name: cat.node.name,
+            to: `/members/${cat.node.slug}`,
+          })),
+        },
+        { name: "Equalities", to: "/equalities" },
+        { name: "Reps", to: "/reps" },
+        { name: "Gallery", to: "/gallery" },
+        { name: "Affiliations", to: "/affiliation" },
+      ]
+      return (
+        <div style={{ background: "teal" }}>
+          <Header
+            siteTitle={data.site.siteMetadata.title}
+            navLinks={navLinks}
+          />
+          <div
+            style={{
+              margin: `0 auto`,
+              maxWidth: 1200,
+              padding: `0px 1.0875rem 1.45rem`,
+              paddingTop: 0,
+            }}
+          >
+            {children}
+            <Sidebar />
+          </div>
+          <Footer navLinks={navLinks} />
         </div>
-        <Footer navLinks={navLinks} />
-      </div>
-    )}
+      )
+    }}
   />
 )
 
