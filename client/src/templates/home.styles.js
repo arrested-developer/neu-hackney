df95c8af33803ecd350c0e55a80fefbab006757c
Michael Watts <hello@michaelwatts.co>
2019-04-24 21:07:55 +0100

    remove surplus styles

--- client/src/templates/home.styles.js
+++ client/src/templates/home.styles.js
@@ -1,25 +1,9 @@
 import styled from "styled-components"
 
-export const HomePageSection = styled.section`
-  margin-bottom: ${({ theme }) => theme.xl};
-`
-
-export const Content = styled.article`
-  color: ${({ theme }) => theme.white};
-`
-
 export const Main = styled.main`
   width: 100%;
   @media ${({ theme }) => theme.break_ns} {
     width: calc(70% - ${({ theme }) => theme.m});
     float: left;
-  }
-`
-
-export const Aside = styled.aside`
-  width: 100%;
-  @media ${({ theme }) => theme.break_ns} {
-    width: calc(30% - ${({ theme }) => theme.m});
-    float: right;
   }
 `
