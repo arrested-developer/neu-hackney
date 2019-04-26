709bee1dc8bf692c3972ec227d98ae635263c9bd
Michael Watts <hello@michaelwatts.co>
2019-04-26 15:51:29 +0100

    display google calendar in events page relates #5

--- client/src/templates/events.js
+++ client/src/templates/events.js
@@ -15,6 +15,17 @@ export default ({ pageContext: { events } }) => {
         <H1>Events</H1>
         <Main>
           <PageSection
+            title="Calendar"
+            titleColor="white"
+            titleBackground="purple"
+          >
+            <iframe
+              width="100%"
+              height="500px"
+              src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;src=en.uk%23holiday%40group.v.calendar.google.com&amp;color=%23125A12&amp;ctz=Europe%2FLondon"
+            />
+          </PageSection>
+          <PageSection
             title="Upcoming Events"
             titleColor="white"
             titleBackground="dark_blue"
