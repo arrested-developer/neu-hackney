37f479cf73cfdd869621500b20a5de7aee146aee
Michael Watts <hello@michaelwatts.co>
2019-05-12 12:22:25 +0200

    add descriptions to block editors

--- backend/wp-content/plugins/neu-hackney/src/newsletters/block.js
+++ backend/wp-content/plugins/neu-hackney/src/newsletters/block.js
@@ -28,6 +28,8 @@ import './editor.scss';
  * @return {?WPBlock}          The block, if it has been successfully
  *                             registered; otherwise `undefined`.
  */
+let warningShown = false;
+
 registerBlockType( 'neu-hackney/newsletter', {
 	// Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
 	title: __( 'NEU Hackney - Newsletter Block' ), // Block title.
@@ -53,12 +55,9 @@ registerBlockType( 'neu-hackney/newsletter', {
 			source: 'meta',
 			meta: 'neuhack_attachment_url',
 		},
-		warningShown: {
-			type: 'boolean',
-		},
 	},
 	edit: ( {
-		attributes: { newsletterID, newsletterURL, warningShown },
+		attributes: { newsletterID, newsletterURL },
 		className,
 		setAttributes,
 	} ) => {
@@ -69,19 +68,18 @@ registerBlockType( 'neu-hackney/newsletter', {
 				__newsletterURL: file.url,
 			} );
 		};
+
 		if ( ! warningShown ) {
 			wp.data.dispatch( 'core/notices' ).createNotice(
 				'warning', // Can be one of: success, info, warning, error.
-				'Newsletter will be published with the current month and year. To publish with a different date, change the publish settings from "Immediately" to your chosen date in the "Status & Visibility" options at the top right of the screen.', // Text string to display.
+				'Newsletter will be published with the current month and year. To publish with a different date, change the publish settings from "Immediately" to your chosen date in the "Status & Visibility" options at the top right of the screen. There is no need to add a title, as this will be set using the selected date', // Text string to display.
 				{
 					isDismissible: true, // Whether the user can dismiss the notice.
 					// Any actions the user can perform.
 					actions: [],
 				}
 			);
-			setAttributes( {
-				warningShown: true,
-			} );
+			warningShown = true;
 		}
 
 		return (
