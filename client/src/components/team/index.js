fa650ba3cd48c0acbf7bcb19fb0b3f9a37fcc2c1
Michael Watts <hello@michaelwatts.co>
2019-05-23 11:15:22 +0100

    responsive sizing for team cards relates #59 #71

--- client/src/components/team/index.js
+++ client/src/components/team/index.js
@@ -3,7 +3,7 @@ import React from "react"
 import TeamCard from "./teamCard"
 import ElectionInfo from "./electionInfo"
 
-import { TeamList } from "./team.styles"
+import { TeamListFlex } from "./team.styles"
 
 const sortByPosition = team => {
   const sortedTeam = team.slice().sort((a, b) => {
@@ -21,12 +21,12 @@ const sortByPosition = team => {
 export default ({ team, noInfo = false, ...props }) => {
   return (
     <>
-      <TeamList {...props}>
+      <TeamListFlex {...props}>
         {sortByPosition(team.edges || team).map(teamMember => {
           if (teamMember.id === undefined) teamMember = teamMember.node
           return <TeamCard key={teamMember.id} teamMember={teamMember} />
         })}
-      </TeamList>
+      </TeamListFlex>
       {noInfo === false && <ElectionInfo />}
     </>
   )
