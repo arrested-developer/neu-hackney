b5afb403f5f6528831d01f39140faee502c54807
Michael W <hello@michaelwatts.co>
2020-07-03 08:22:46 +0100

    Update events.js

--- client/src/templates/events.js
+++ client/src/templates/events.js
@@ -24,7 +24,7 @@ export default ({ pageContext: { events, settings } }) => {
               title="Google Calendar"
               width="100%"
               height="500px"
-              src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;src=en.uk%23holiday%40group.v.calendar.google.com&amp;color=%23125A12&amp;ctz=Europe%2FLondon"
+              src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;src=75s309ejsqomoli3je3k8rb654%40group.calendar.google.com&ctz=Europe%2FLondon"
             />
           </PageSection>
           <PageSection
